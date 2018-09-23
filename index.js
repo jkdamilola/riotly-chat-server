a12017a1e4c11e4eda9ac59c142166aa90713db4
Jimoh Damilola <jkdamilola@gmail.com>
2018-09-23 23:20:09 +0100

    make it better

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ if (config.mongooseDebug) {
 // src: https://github.com/mochajs/mocha/issues/1912
 if (!module.parent) {
   // listen on port config.port
+  console.log('config.port', config.port);
   app.listen(config.port, () => {
     console.info(`server started on port ${config.port} (${config.env})`); // eslint-disable-line no-console
   });
